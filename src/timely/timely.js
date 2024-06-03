cda08bb8ba642e5fdea1108e31568632620812b2
ike-jide <ikejideofor@gmail.com>
2024-06-03 16:12:55 +0100

    made ui dynamic

---
+++ src/timely/timely.js
@@ -0,0 +1,33 @@
+const create = document.querySelector(".newSchedule")
+const view = document.querySelector(".viewEmails")
+const table = document.querySelector(".table")
+
+create.addEventListener('click',()=>{
+
+})
+view.addEventListener('click',()=>{
+    updateUi()
+})
+//update ui
+ 
+const updateUi =()=>{
+    const html=
+    `
+    <tr>
+      <th>Title</th>
+      <th>Message</th>
+      <th>Time</th>
+    </tr>
+    <tr>
+      <td>School application</td>
+      <td>this is regarding your addmission</td>
+      <td>12:00pm</td>
+    </tr>
+    <tr>
+      <td>Deadline</td>
+      <td>deadline has passed</td>
+      <td>11:20am</td>
+    </tr>
+  `
+  table.innerHTML +=html
+}
